e394f03a6b1de45da08f2e47ebedb9dd479f3b4d
GaEunJang <22100628@handong.ac.kr>
2022-04-18 20:22:40 +0900

    selectMenu 5번 6번 수정

--- product.c
+++ product.c
@@ -59,6 +59,8 @@ int selectMenu(){
     printf("2.추가\n");
     printf("3.수정\n");
     printf("4.삭제\n");
+    printf("5.파일저장\n");
+    printf("6..과일이름 검색\n");
     printf("0.종료\n\n");
     printf("=>원하는 메뉴는? ");
     scanf("%d",&menu);
