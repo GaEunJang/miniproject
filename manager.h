dfb05f648ddabeb011a1d713388fa9835f7cb53c
GaEunJang <22100628@handong.ac.kr>
2022-04-18 20:18:01 +0900

    제품 중량 검색 기능 함수 추가

--- manager.h
+++ manager.h
@@ -7,3 +7,4 @@ int loadData(Product *s); //File에서 데이터 불러오는 함수
 void searchName(Product *s,int count); //제품이름을 검색하는 함수
 void listProduct(Product *s,int count); //전체 등록된 제품 리스트를 출력하는 함수
 void searchExplain(Product *s,int count); //제품 설명을 검색하는 함수
+void searchWeight(Product *s,int count);//제품 중량을 검색하는 함수
